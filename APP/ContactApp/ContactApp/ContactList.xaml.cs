4c10176a62db47c10bfab2560c01c98b613f7950
Claire Lapointe <clapointe111@gmail.com>
2017-06-23 22:37:51 +0200

    début swipe

--- APP/ContactApp/ContactApp/ContactList.xaml.cs
+++ APP/ContactApp/ContactApp/ContactList.xaml.cs
@@ -10,10 +10,12 @@ namespace ContactApp
 {
     public partial class ContactList : ContentPage
     {
+        float SwipeDistance = 0;
+        float ReferenceSwipeDistance = 30;
+
         public ContactList()
         {
             InitializeComponent();
-
 
             this.ContactListLayout.Children.Add(CreateContactElement(new Contact
             {
@@ -35,7 +37,7 @@ namespace ContactApp
             }));
         }
 
-        public View CreateContactElement(Contact contact)
+        protected View CreateContactElement(Contact contact)
         {
             var contactElement = new StackLayout { Orientation = StackOrientation.Horizontal };
             var elem = new StackLayout { Orientation = StackOrientation.Vertical };
@@ -48,7 +50,47 @@ namespace ContactApp
             button = new Button { Text = "TEL", HorizontalOptions = LayoutOptions.Fill };
             contactElement.Children.Add(button);
 
+            var panGesture = new PanGestureRecognizer();
+            panGesture.PanUpdated += OnPanUpdated;
+            contactElement.GestureRecognizers.Add(panGesture);
+
             return contactElement;
+        }
+
+        void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    //HandleTouchStart();
+                    break;
+                case GestureStatus.Running:
+                    HandleTouch((View)sender, (float)e.TotalX);
+                    break;
+                case GestureStatus.Completed:
+                    HandleTouchEnd((View)sender);
+                    break;
+            }
+        }
+
+        async void HandleTouch(View sender, float diff_x)
+        {
+            await sender.TranslateTo(diff_x, 0);
+            SwipeDistance = diff_x;
+        }
+
+        async void HandleTouchEnd(View sender)
+        {
+            //if(Math.Abs(SwipeDistance) > ReferenceSwipeDistance)
+            //{
+            //  // envoyer l'élément hors du champ
+            //}
+            //else
+            {
+                // remettre l'élément à sa position initiale
+                await sender.TranslateTo(-sender.X, 0);
+                SwipeDistance = 0;
+            }
         }
     }
 }
